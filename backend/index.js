526dc576badcf343a93c8dbb8d2e634f34be5c97
imverma09 <imverma45@gmail.com>
2025-01-29 22:11:35 +0530

    post method using form data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 const express =  require('express')
+const cors = require('cors')
 const app =  express()
 const port = 5000 ; 
+
+app.use(cors({
+  origin : "http://localhost:3000"
+}))
+
+app.use(express.json())
 
 app.get("/" ,(req ,res)=>{
     const query =   req.query
     console.log(query.app)
     res.status(200).send("all good")
+})
+
+app.post("/logindata" ,(req, res)=>{
+  const data = req.body
+  console.log(data)
+   res.json({"messages" : "dataSave"})
 })
 
 app.listen(port, ()=>{
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  
+  function handleInput(e) {
+    const { id, value } = e.target;
+    setUserData({ ...userData, [id]: value });
+  }
+  const  handleSubmit = async (e)=> {
+    console.log('click')
+    e.preventDefault();
+    try {
+      const res =  await fetch("http://localhost:5000/logindata", {
+        method: "post",
+        body: JSON.stringify(userData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      const  data =  await res.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+      
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          id="name"
+          value={userData.name}
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          value={userData.email}
+          onChange={handleInput}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          value={userData.password}
+          onChange={handleInput}
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
