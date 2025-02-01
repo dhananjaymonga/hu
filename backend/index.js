a143ffeadffe02fe2c0747ad29d5a6c888891e0e
imverma09 <imverma45@gmail.com>
2025-02-01 20:41:45 +0530

    add bcrypt

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const users = require("./model/user");
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const app = express();
 const port = 5000;
-const random = "1lk3kelo3kjo3i8409403jKKWJDIC#@)$*@(r93"
+const random = "4j94jr02kjei50j34903i05030"
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -14,7 +15,6 @@ app.use(
 );
 
 app.use(express.json());
-
 app.get("/", (req, res) => {
   const query = req.query;
   console.log(query.app);
@@ -23,13 +23,18 @@ app.get("/", (req, res) => {
 
 app.post("/logindata", async (req, res) => {
   const data = req.body;
+  {}
   try {
+    const salt =  await bcrypt.genSalt()
+    const newPassword = await bcrypt.hash(data.password , salt)
+    data.password = newPassword
     const user =  await users.create(data);
-    const token =  jwt.sign({_id : user._id} , random )
-    res.cookie('myID' , token , {
-      httpOnly : true ,
-      maxAge : 15 * 60 * 1000
+    const token = jwt.sign({ _id : user._id}, random)
+    res.cookie('myId', token ,{
+      httpOnly : true,
+      maxAge: 15 * 60 * 1000
     })
+    
     res.status(200).json({ msg: "Data Save" , success : true });
   } catch (error) {
     res.status(500).json({ msg: "sever error" , success : false });
